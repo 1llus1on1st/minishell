0fabcdbfe2c2d6f4588b7880b0caf06d1a17e6bf
mshargan <mshargan@student.42.fr>
2026-07-10 11:20:50 +0200

    tester lvl1 fixes

--- src/expand/expand_marked_word.c
+++ src/expand/expand_marked_word.c
@@ -6,7 +6,7 @@
 /*   By: mshargan <mshargan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 12:41:15 by mshargan          #+#    #+#             */
-/*   Updated: 2026/07/10 11:12:07 by mshargan         ###   ########.fr       */
+/*   Updated: 2026/07/10 11:20:47 by mshargan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,19 @@ static int	copy_single_marked(t_shell *shell, char *str, char **res, int *i)
 	if (str[*i] == '\'')
 		(*i)++;
 	return (1);
+}
+
+static int	copy_escaped_marked(t_shell *shell, char *str, char **res, int *i)
+{
+	if (str[*i + 1])
+	{
+		(*i)++;
+		if (!append_char(shell, res, str[*i]))
+			return (0);
+		(*i)++;
+		return (1);
+	}
+	return (append_char(shell, res, str[(*i)++]));
 }
 
 static int	copy_double_marked(t_shell *shell, char *str, char **res, int *i)
@@ -58,18 +71,6 @@ static int	copy_dollar_quote_marked(t_shell *shell, char *str,
 	if (str[*i] == '"')
 		return (copy_double_marked(shell, str, res, i));
 	return (expand_unquoted_variable(shell, str, res, i));
-}
-static int	copy_escaped_marked(t_shell *shell, char *str, char **res, int *i)
-{
-	if (str[*i + 1])
-	{
-		(*i)++;
-		if (!append_char(shell, res, str[*i]))
-			return (0);
-		(*i)++;
-		return (1);
-	}
-	return (append_char(shell, res, str[(*i)++]));
 }
 
 static int	handle_marked_char(t_shell *shell, char *str, char **res, int *i)
